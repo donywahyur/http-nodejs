3c2738e6156cf85be89231a3eca7f595e4318301
donywahyur <donyramadhan3011@gmail.com>
2023-01-11 09:58:53 +0700

    modified:   server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,18 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
+  const mysql = require('mysql');
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'database name'
+  });
+  connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+  });
+
   res.write('Hallo!');
   res.end();
 }).listen(process.env.PORT);
